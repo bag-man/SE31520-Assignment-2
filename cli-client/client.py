94d4de0a9af442b9d698651cc41dc8539bfd9424
Owen Garland <owen.garland@clock.co.uk>
2016-11-01 16:56:39 +0000

    Got a CLI, posting doesn't work because of InvalidAuthenticityToken

--- cli-client/client.py
+++ cli-client/client.py
@@ -1,25 +1,45 @@
+"""
+client.py
+Usage:
+  client.py (--user=<USER:PASS>) [-j]
+  client.py (--user=<USER:PASS>) [--post=<POST_DATA>]
+  client.py --help
+Options:
+  -h --help                        Display this help message
+  -j --json                        Print raw JSON
+  -p CONTENT,   --post  CONTENT    Make a new broadcast
+  -u USER:PASS, --user  USER:PASS  Specify a username and password
+"""
+
+from docopt import docopt
 import requests
-import json
+import sys
 
-'''
-{
- "users": [
-    {
-      "username": "John",
-      "email": "test@gmail.com"
-    },
-    {
-      "username": "Sarah",
-      "email": "tarah@gmail.com"
-    }
-  ]
-}
-'''
+def get(json):
+    endpoint = 'http://localhost:3000/broadcasts.json'
+    response = requests.get(endpoint, auth=('admin', 'taliesin'))
+    data = response.json()
 
-endpoint = 'http://demo0361004.mockable.io/users'
+    if json:
+        print(data)
+    else:
+        print('Formatted data')
 
-response = requests.get(endpoint)
+def post(data):
+    endpoint = 'http://localhost:3000/broadcasts'
+    postdata = { 'content': data }
+    response = requests.post(endpoint, auth=('admin', 'taliesin'), params=postdata)
+    data = response.content
+    # curl --user admin:talieson http://localhost:3000/broadcasts --data content=test > error.htm
+    # InvalidAuthenticityToken
+    print(data)
 
-data = response.json()
-
-print(data)
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    if args['--help']:
+        print(args.help_message)
+        sys.exit(0)
+    if args['--post']:
+        post(args['--post'])
+    else:
+        get(args['--json'])
