b8d60801ad8147ce75c8078e8e5e79f7ed1470a5
Owen Garland <owen.garland@clock.co.uk>
2016-11-18 14:06:35 +0000

    Some cucumber progress

--- features/step_definitions/notification-feed_steps.rb
+++ features/step_definitions/notification-feed_steps.rb
@@ -1,31 +1,42 @@
-session = Capybara::Session.new(:selenium)
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
+DatabaseCleaner.strategy = :truncation
+
+Before do
+  DatabaseCleaner.start
+  user = User.create!(surname: 'Admin', firstname: 'Admin', email: 'admin@aber.ac.uk', phone: '07000 000000', grad_year: 2016)
+  UserDetail.create!(login: 'admin', password: 'admin', user: user)
+end
+
+After do |scenario|
+  DatabaseCleaner.clean
+end
+
 
 Given(/^that user "([^"]*)" with password "([^"]*)" has logged in$/) do |arg1, arg2|
-  visit("/session/new") do
-    within('form') do
-      session.fill_in 'Login', with: arg1
-      session.fill_in 'Password', with: arg2
-      session.click_button 'Login'
-    end
+  visit("/session/new")
+  within('.session-form') do
+    fill_in 'login', with: arg1
+    fill_in 'password', with: arg2
+    click_button 'Login'
   end
+  puts(find('.flash_message').text)
 end
 
 When(/^the admin user broadcasts "([^"]*)" to the notification feed$/) do |arg1|
-  visit("/broadcasts/new") do
-    within('form') do
-      session.fill_in 'broadcast_content', with: arg1
-      session.click_button 'Broadcast'
-    end
+  visit("/broadcasts/new")
+  within('.broadcast-form') do
+    fill_in 'broadcast_content', with: arg1
+    click_button 'Broadcast'
   end
 end
 
 Then(/^the current page should contain the text: "([^"]*)"$/) do |arg1|
-  # puts(session.find('.flash_notice').text)
-  puts(session.find('h1').text)
+  puts(find('.flash_message').text)
   pending
 end
 
 Then(/^the current page should show the text "([^"]*)" in the notification feed$/) do |arg1|
-  pending
+  # pending
 end
 
